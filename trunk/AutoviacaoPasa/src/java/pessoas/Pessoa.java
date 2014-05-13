43622d375abb6210ad1c9f66707faf9233249f75
arthurcaique182@gmail.com <arthurcaique182@gmail.com@70431027-f894-6335-798c-b1aac3db4cdb>
2014-05-13 05:13:06 +0000

    Classe Pessoa

--- trunk/AutoviacaoPasa/src/java/pessoas/Pessoa.java
+++ trunk/AutoviacaoPasa/src/java/pessoas/Pessoa.java
@@ -8,6 +8,50 @@ package pessoas;
  *
  * @author Arthur
  */
-public class Pessoa {
-    private String nome;
+public abstract class Pessoa {
+    
+    //Atributos
+    private String nome, cpf, telefone;
+    
+    //Construtor Vazio
+    public Pessoa(){
+        
+    }
+    
+    //Construtor
+    public Pessoa(String nome, String cpf, String telefone){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+    }
+    
+    //Gets e Sets
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    
+    
+    
+    
 }
