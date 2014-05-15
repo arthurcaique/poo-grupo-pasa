0c84fa0e769b372a6240539365cd77e4c5384d8a
arthurguilher <arthurguilher@gmail.com>
2014-05-15 04:53:00 +0000

    Repositorio de Onibus

---
+++ AutoviacaoPasa/src/java/onibus/RepositorioOnibus.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package onibus;
+
+import index.ErroInternoException;
+
+/**
+ *
+ * @author Arthur
+ */
+public interface RepositorioOnibus {
+    
+    public void cadastrar(Onibus onibus) throws ErroInternoException, OnibusExistenteException;
+    
+    public void excluir(long id) throws ErroInternoException, OnibusInexistenteException;
+    
+    public void editar(Onibus onibus) throws ErroInternoException, OnibusInexistenteException;
+}
