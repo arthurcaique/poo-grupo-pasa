0ddc2168f2133dc43402f07ab161af4bb6a90f70
arthurcaique182 <arthurcaique182@gmail.com>
2014-05-15 05:12:25 +0000

    Classe Cliente

--- AutoviacaoPasa/src/java/clientes/Cliente.java
+++ AutoviacaoPasa/src/java/clientes/Cliente.java
@@ -6,7 +6,12 @@
 
 package clientes;
 
+import java.io.Serializable;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import pessoas.Pessoa;
 
 /**
@@ -14,6 +19,41 @@ import pessoas.Pessoa;
  * @author Arthur
  */
 @Entity
-public class Cliente extends Pessoa {
+@Table (name = "clientes")
+public class Cliente extends Pessoa implements Serializable {
+    //Atributos
+    private long id_cliente;
+    private String senha;
+    
+    //Construtor Vazio
+    public Cliente(){
+        
+    }
+    
+    //Construtor
+    public Cliente( String nome, String cpf, String telefone, long id_cliente, String senha){
+        super(nome, cpf, telefone);
+        this.id_cliente = id_cliente;
+        this.senha = senha;   
+    }
+    
+    //Gets e Sets
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(long id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
     
 }
