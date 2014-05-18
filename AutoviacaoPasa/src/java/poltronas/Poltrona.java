c86192f4d5ff6f2931f3ca6c5aa1060ea4ff964d
arthurcaique182 <arthurcaique182@gmail.com>
2014-05-18 01:32:53 +0000

    Entidade Poltrona

--- AutoviacaoPasa/src/java/poltronas/Poltrona.java
+++ AutoviacaoPasa/src/java/poltronas/Poltrona.java
@@ -6,10 +6,45 @@
 
 package poltronas;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 /**
  *
  * @author Arthur
  */
-public class Poltrona {
+@Entity
+public class Poltrona implements Serializable{
+    private long id_poltrona;
+    private int numero_poltrona;
+    
+    public Poltrona(){
+        
+    }
+    
+    public Poltrona(long id_poltrona, int numero_poltrona){
+        this.id_poltrona = id_poltrona;
+        this.numero_poltrona = numero_poltrona;
+    }
+    
+    @Id
+    public long getId_poltrona() {
+        return id_poltrona;
+    }
+
+    public void setId_poltrona(long id_poltrona) {
+        this.id_poltrona = id_poltrona;
+    }
+    
+    @Column(unique = true)
+    public int getNumero_poltrona() {
+        return numero_poltrona;
+    }
+
+    public void setNumero_poltrona(int numero_poltrona) {
+        this.numero_poltrona = numero_poltrona;
+    }
     
 }
