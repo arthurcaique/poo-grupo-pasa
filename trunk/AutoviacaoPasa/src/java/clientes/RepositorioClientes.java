95cd6c556998ecdbbea00bd417312db969141fc6
arthurcaique182@gmail.com <arthurcaique182@gmail.com@70431027-f894-6335-798c-b1aac3db4cdb>
2014-05-18 01:15:41 +0000

    Pacote Cliente - Repositorio, Exception

---
+++ trunk/AutoviacaoPasa/src/java/clientes/RepositorioClientes.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientes;
+
+import index.ErroInternoException;
+
+/**
+ *
+ * @author Arthur
+ */
+public interface RepositorioClientes {
+    
+    public void adicionar (Cliente c) throws ErroInternoException;
+    public void remover (long id_cliente) throws ErroInternoException, ClienteInexistenteException;
+    public Cliente buscar (long id_cliente) throws ErroInternoException, ClienteInexistenteException;
+    public void atualizar (Cliente c) throws ErroInternoException, ClienteInexistenteException;
+}
