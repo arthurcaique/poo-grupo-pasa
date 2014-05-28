c673b79694a238b53c5ad694f388d0655cf29845
arthurguilher@gmail.com <arthurguilher@gmail.com@70431027-f894-6335-798c-b1aac3db4cdb>
2014-05-28 20:36:26 +0000

    RepositorioOnibusJPA

--- trunk/AutoviacaoPasa/src/java/onibus/RepositorioOnibusJPA.java
+++ trunk/AutoviacaoPasa/src/java/onibus/RepositorioOnibusJPA.java
@@ -3,13 +3,66 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package onibus;
+
+import index.ErroInternoException;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
 
 /**
  *
  * @author Arthur
  */
-public class RepositorioOnibusJPA implements RepositorioOnibus{
+public class RepositorioOnibusJPA implements RepositorioOnibus {
+
+    private EntityManager em;
+
+    public RepositorioOnibusJPA() {
+        this.em = Persistence.createEntityManagerFactory("autoviacao_pasa").createEntityManager();
+    }
+
+    public void cadastrar(Onibus onibus) throws ErroInternoException {
+        try {
+            this.em.persist(onibus);
+        } catch (Exception e) {
+            throw new ErroInternoException(e);
+        }
+    }
+
+    public void excluir(long id_onibus) throws ErroInternoException, OnibusInexistenteException {
+        try {
+            Onibus onibus = this.em.find(Onibus.class, id_onibus);
+            if (onibus == null) {
+                throw new OnibusInexistenteException();
+            }
+            this.em.remove(onibus);
+        } catch (Exception e) {
+            throw new ErroInternoException(e);
+        }
+    }
     
+    public Onibus buscar(long id_onibus) throws ErroInternoException, OnibusInexistenteException {
+        try{
+            Onibus onibus = this.em.find(Onibus.class, id_onibus);
+            if (onibus == null) {
+                throw new OnibusInexistenteException();
+            }
+            return onibus;
+        } catch (Exception e) {
+            throw new ErroInternoException(e);
+        }
+    }
+    
+    public void editar(Onibus onibus) throws ErroInternoException, OnibusInexistenteException {
+        try{
+            Onibus onibus2 = this.em.find(Onibus.class, onibus.getId_onibus());
+            if (onibus2 == null) {
+                throw new OnibusInexistenteException();
+            }
+            this.em.refresh(onibus2);
+        } catch (Exception e) {
+            throw new ErroInternoException(e);
+        }
+    }
+
 }
