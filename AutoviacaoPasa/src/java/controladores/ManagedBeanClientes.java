72c3c6c9be8ed7b5c18c1f037ef0af1755dae6b3
arthurguilher <arthurguilher@gmail.com>
2014-06-17 23:02:32 +0000

    Correção ManagedCliente

--- AutoviacaoPasa/src/java/controladores/ManagedBeanClientes.java
+++ AutoviacaoPasa/src/java/controladores/ManagedBeanClientes.java
@@ -10,11 +10,11 @@ import clientes.ClienteInexistenteException;
 import index.ErroInternoException;
 import index.Fachada;
 import java.io.Serializable;
+import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
-
 
 /**
  *
@@ -23,11 +23,16 @@ import javax.faces.context.FacesContext;
 @ManagedBean
 @RequestScoped
 public class ManagedBeanClientes implements Serializable {
+
+    @EJB
     private Fachada fachada;
     private Cliente cliente;
-    
-    public ManagedBeanClientes(){
-        this.cliente = new Cliente();
+
+    public ManagedBeanClientes() {
+    }
+
+    public ManagedBeanClientes(Cliente cliente) {
+        this.cliente = cliente;
     }
   
 
@@ -38,64 +43,59 @@ public class ManagedBeanClientes implements Serializable {
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
     }
-    
-    public String adicionarCliente(){
-         try{
+
+    public String adicionarCliente() {
+        try {
             this.fachada.adicionar(this.cliente);
             FacesContext contexto = FacesContext.getCurrentInstance();
             FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Ok", "Cliente Cadastrado com sucesso");
-            contexto.addMessage(null, msg);                          
-        }
-        catch(ErroInternoException eie){
+            contexto.addMessage(null, msg);
+            return "index.xhtml";
+
+        } catch (ErroInternoException eie) {
             FacesContext contexto = FacesContext.getCurrentInstance();
             FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Erro Interno", "Ocorreu um errointerno inesperado!");
             contexto.addMessage(null, msg);
-        }
-        catch(ClienteExistenteException cee){
+            return null;
+        } catch (ClienteExistenteException cee) {
             FacesContext contexto = FacesContext.getCurrentInstance();
             FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Cliente Existente", "Cliente já existe");
             contexto.addMessage(null, msg);
+            return null;
         }
-        return null;
     }
-    
-    public String removerCliente(){
-        try{
-        this.fachada.remover(cliente.getId_cliente());
-        return "index.xhtml";
-        }
-        catch(ErroInternoException eie){
+
+    public String removerCliente() {
+        try {
+            this.fachada.remover(cliente.getId_cliente());
+            return "index.xhtml";
+        } catch (ErroInternoException eie) {
             return "ErroInterno.xhtml";
-        }
-        catch(ClienteInexistenteException cie){
+        } catch (ClienteInexistenteException cie) {
             return "ClienteInexistente.xhtml";
         }
     }
-    
-    public String buscarCliente(){
-        try{
+
+    public String buscarCliente() {
+        try {
             this.fachada.buscarCliente(cliente.getId_cliente());
             return "InformacoesCliente.xhtml";
-        }
-        catch(ErroInternoException eie){
+        } catch (ErroInternoException eie) {
             return "ErroInterno.xhtml";
-        }
-        catch(ClienteInexistenteException cie){
+        } catch (ClienteInexistenteException cie) {
             return "ClienteInexistente.xhtml";
         }
     }
-    
-    public String atualizarCliente(){
-        try{
+
+    public String atualizarCliente() {
+        try {
             this.fachada.atualizar(cliente);
             return "index.xhtml";
-        }
-        catch(ErroInternoException eie){
+        } catch (ErroInternoException eie) {
             return "ErroInterno.xhtml";
-        }
-        catch(ClienteInexistenteException cie){
+        } catch (ClienteInexistenteException cie) {
             return "ClienteInexistente.xhtml";
         }
     }
-        
+
 }
