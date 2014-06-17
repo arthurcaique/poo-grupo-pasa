774c06e2f7a5bcc46f458a5bac459a35d1616561
arthurguilher@gmail.com <arthurguilher@gmail.com@70431027-f894-6335-798c-b1aac3db4cdb>
2014-06-17 23:02:32 +0000

    Correção ManagedCliente

--- trunk/AutoviacaoPasa/src/java/controladores/ManagedBeanOnibus.java
+++ trunk/AutoviacaoPasa/src/java/controladores/ManagedBeanOnibus.java
@@ -8,7 +8,10 @@ package controladores;
 import index.ErroInternoException;
 import index.Fachada;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 import onibus.Onibus;
 import onibus.OnibusExistenteException;
 
@@ -17,6 +20,7 @@ import onibus.OnibusExistenteException;
  * @author Arthur
  */
 @ManagedBean
+@RequestScoped
 public class ManagedBeanOnibus {
 
     @EJB
@@ -25,13 +29,28 @@ public class ManagedBeanOnibus {
 
     public ManagedBeanOnibus() {
     }
+    
+    public ManagedBeanOnibus(Onibus onibus){
+        this.onibus = onibus;
+    }
 
     public String cadastrarOnibus() throws OnibusExistenteException, ErroInternoException {
         try {
+            FacesContext contexto = FacesContext.getCurrentInstance();
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Ok", "Cliente Cadastrado com sucesso");
+            contexto.addMessage(null, msg);
             fachada.cadastrar(onibus);
             return "index.xhtml";
-        } catch (Exception e) {
-            //throw new ErroInternoException(e);
+        }catch(ErroInternoException eie){
+            FacesContext contexto = FacesContext.getCurrentInstance();
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Erro Interno", "Ocorreu um errointerno inesperado!");
+            contexto.addMessage(null, msg);
+            return null;
+        }
+        catch(OnibusExistenteException cee){
+            FacesContext contexto = FacesContext.getCurrentInstance();
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Onibus Existente", "Onibus já existe");
+            contexto.addMessage(null, msg);
             return null;
         }
     }
