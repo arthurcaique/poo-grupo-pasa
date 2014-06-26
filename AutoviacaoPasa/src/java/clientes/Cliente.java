418704c674915caa796269f3287f5dbcb228c179
arthurguilher <arthurguilher@gmail.com>
2014-06-26 02:36:12 +0000

    Sessão de login funcionando e correção em todas as páginas

--- AutoviacaoPasa/src/java/clientes/Cliente.java
+++ AutoviacaoPasa/src/java/clientes/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 /**
  *
@@ -86,5 +87,9 @@ public class Cliente implements Serializable {
         this.telefone = telefone;
     }
     
+    @Override
+    public String toString(){
+        return (this.nome);
+    }
     
 }
